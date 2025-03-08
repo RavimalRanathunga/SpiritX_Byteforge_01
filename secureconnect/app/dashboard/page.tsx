430f274f1ddd98a20334c78c7a433462cfd16cd1
RavimalRanathunga <ravimalranathunga@gmail.com>
2025-03-08 09:03:49 +0530

    add middleware and nextconfig

---
+++ secureconnect/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard()
+{
+    return(
+        <div className="flex justify-center items-center">
+            <h1>Good Luck You have Logged In Succesfully</h1>
+        </div>
+    );
+}
