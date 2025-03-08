b131658e0e0d930c7e19bfca091330f5ac27f514
RavimalRanathunga <ravimalranathunga@gmail.com>
2025-03-08 09:46:10 +0530

    add bcrypt package

--- secureconnect/auth.ts
+++ secureconnect/auth.ts
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
 
 export const {auth,signIn,signOut}=NextAuth({
     ...authConfig,
-    providers:[Credentials({})],
-})
+    providers:[Credentials({
+        async authorize(credentials){
+            const Credentials =z.object({username:z.string(),password:z.string.min(8)}).safeParse(credentials);
+        },
+    }),
+],
+});
